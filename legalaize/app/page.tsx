3af81008d083d9ac6b4711aecf871d03e46f1fd2
suneettipirneni <suneettipirneni@icloud.com>
2023-04-29 01:18:38 -0400

    more realized ui WOW

--- legalaize/app/page.tsx
+++ legalaize/app/page.tsx
@@ -4,10 +4,46 @@ import { ChatUI } from "@/components/Chat";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const sideItems = [
+  "Foobar.docx",
+  "Baz.docx",
+  "Qux.docx",
+  "Quux.docx",
+  "Quuz.docx",
+  "Corge.docx",
+  "Grault.docx",
+];
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between max-h-screen">
-      <div className="z-10 grow max-w-5xl items-center justify-between flex w-full min-h-screen">
+    <main className="flex flex-row justify-center max-h-screen bg-white">
+      <div className="flex text-black border-r border-slate-200 max-w-[400px] min-w-[100px] items-center flex-col p-5 space-y-5">
+        <h1 className="font-bold text-3xl w-full">Documents</h1>
+        <div className="space-y-2 w-full overflow-auto">
+          {sideItems.map((item, idx) => (
+            <div
+              key={idx}
+              className="flex flex-col items-start space-y-1 w-full max-h-[200px] bg-slate-100 rounded-xl p-2 hover:bg-slate-50"
+            >
+              <span className="font-lg font-bold">{item}</span>
+              <span className="grow font-mono text-sm text-ellipsis break-words line-clamp-4 max-w-full">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Est
+                lorem ipsum dolor sit. Quis blandit turpis cursus in hac.
+                Sollicitudin nibh sit amet commodo nulla facilisi nullam.
+                Adipiscing commodo elit at imperdiet dui accumsan. Massa vitae
+                tortor condimentum lacinia quis vel. Etiam non quam lacus
+                suspendisse. Semper viverra nam libero justo laoreet sit amet.
+                Egestas fringilla phasellus faucibus scelerisque. Condimentum
+                vitae sapien pellentesque habitant morbi tristique senectus et.
+                Turpis massa tincidunt dui ut ornare. Tristique nulla aliquet
+                enim tortor at.
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="z-10 grow items-center justify-center flex w-full min-h-screen">
         <ChatUI />
       </div>
     </main>
--- legalaize/components/Chat.tsx
+++ legalaize/components/Chat.tsx
@@ -2,45 +2,110 @@
 
 import { useState } from "react";
 
+import { BsFillCursorFill } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { MoonLoader } from "react-spinners";
+
+interface Message {
+  content: string;
+  recieved: boolean;
+}
+
 export function ChatBubble({
   text,
-  className,
+  recieved = false,
 }: {
   text: string;
-  className?: string;
+  recieved?: boolean;
 }) {
+  const alignClass = recieved ? "self-start" : "self-end";
+  const bgClass = recieved
+    ? "bg-slate-100 text-black"
+    : "bg-blue-500 shadow-sm text-white";
   return (
-    <div className={`p-2 bg-blue-500 rounded-xl ${className}`}>{text}</div>
+    <motion.div
+      initial={{ opacity: 0, x: 100, scale: 0.7, rotate: 5 }}
+      animate={{ opacity: 1, x: 0, scale: 1, rotate: 0 }}
+      transition={{
+        duration: 0.5,
+        ease: "easeOut",
+        type: "spring",
+        damping: 5,
+        stiffness: 70,
+        restDelta: 0.1,
+      }}
+      className={`py-2 px-3 ${bgClass} rounded-xl min-w-[40px] text-lg text-left max-w-[60%] ${alignClass}`}
+    >
+      {text}
+    </motion.div>
   );
 }
 
-export function ChatUI() {
-  const [chats, setChats] = useState<string[]>([]);
+const messages: Message[] = [
+  {
+    content: "Hello",
+    recieved: true,
+  },
+  {
+    content: "Hi",
+    recieved: false,
+  },
+  {
+    content: "How are you?",
+    recieved: true,
+  },
+  {
+    content: "Good",
+    recieved: false,
+  },
+];
 
+export function ChatUI() {
+  const [chats, setChats] = useState<Message[]>(messages);
   const [currentChat, setCurrentChat] = useState<string>("");
+  const [thinking, setThinking] = useState<boolean>(false);
 
   return (
     <div
-      className={`flex grow flex-col justify-between space-y-2 border-2 border-red h-screen`}
+      className={`flex grow flex-col justify-between h-screen max-w-[1200px]`}
     >
-      <div className="flex grow flex-col overflow-scroll space-y-2 flex-end">
+      <div className="grow"></div>
+      <div className="flex flex-col overflow-y-auto overflow-x-clip space-y-2 self-end pb-2 px-2 w-full">
         {chats.map((chat, idx) => (
-          <ChatBubble key={idx} text={chat} className="self-end" />
+          <ChatBubble key={idx} text={chat.content} recieved={chat.recieved} />
         ))}
       </div>
 
-      <div className="flex flex-row space-x-4 max-h-[100px]">
+      <div className="flex flex-row space-x-4 max-h-[100px] p-2">
         <input
           name="test"
-          className="text-black grow rounded-full p-3"
+          className="text-black grow rounded-full px-3 py-2 border border-slate-300"
           onChange={(a) => setCurrentChat(a.target.value)}
+          value={currentChat}
         />
-        <button
-          className="rounded-lg bg-blue-400 p-4"
-          onClick={() => setChats([...chats, currentChat])}
-        >
-          Send
-        </button>
+        {thinking ? (
+          <MoonLoader
+            color="blue"
+            loading={true}
+            size={28}
+            speedMultiplier={0.7}
+          />
+        ) : (
+          <button
+            className="rounded-full bg-blue-500 p-4"
+            disabled={currentChat.length === 0}
+            onClick={() => {
+              setChats([...chats, { content: currentChat, recieved: false }]);
+              setCurrentChat("");
+              setThinking(true);
+              setTimeout(() => {
+                setThinking(false);
+              }, 3000);
+            }}
+          >
+            <BsFillCursorFill />
+          </button>
+        )}
       </div>
     </div>
   );
